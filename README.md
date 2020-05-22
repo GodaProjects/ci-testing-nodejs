b39abfbb8e68a1d4bd006ad9a061535547fe69c8
goda <aryaputra.sarangadevi@gmail.com>
2020-05-22 18:19:54 +0530

    Build should pass now

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # travis-ci-testing
 [![Build Status](https://travis-ci.com/GodaProjects/travis-ci-testing.svg?branch=master)](https://travis-ci.com/GodaProjects/travis-ci-testing)
+
 Testing the integration with travis CI
--- test/test.js
+++ test/test.js
@@ -4,7 +4,7 @@ const assert = require('assert')
 describe('Goda', function () {
     describe('Tanu', function () {
         it('Should return -1 when the value is not present', function () {
-            assert.equal(['goda', 'tanu', 'rama'].indexOf(4), 1)
+            assert.equal(['goda', 'tanu', 'rama'].indexOf(4), -1)
         })
     })
 })
